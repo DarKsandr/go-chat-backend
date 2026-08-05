b2d73433a77233079097645123a8a443a4e33426
volkov <gdarksandr@gmail.com>
2026-08-05 21:26:20 +0400

    add websocket

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,32 +2,42 @@ package main
 
 import (
 	"chat/pkg"
-	"log"
+	"encoding/json"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
+
+var hub *Hub
+var db *gorm.DB
 
 func main() {
 	pkg.Init()
 
 	port := os.Getenv("PORT")
 
-	r := gin.Default()
+	router := gin.Default()
+
+	hub = newHub()
+	go hub.run()
+
+	db = pkg.OpenDB()
 
 	{
-		api := r.Group("/api")
+		api := router.Group("/api")
 		api.GET("/message", GetMessageHandler)
 		api.POST("/message", CreateMessageHandler)
 	}
 
-	log.Fatalln(r.Run(":" + port))
+	router.GET("/ws", serveWs)
+
+	router.Run(":" + port)
 }
 
 func GetMessageHandler(c *gin.Context) {
 	messages := []*pkg.Message{}
-	db := pkg.OpenDB()
 	err := db.Scopes(pkg.Paginate(c.Request)).Find(&messages).Error
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -45,12 +55,14 @@ func CreateMessageHandler(c *gin.Context) {
 		return
 	}
 
-	db := pkg.OpenDB()
 	err = db.Create(&message).Error
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+
+	jsonData, err := json.Marshal(message)
+	hub.broadcast <- jsonData
 
 	c.JSON(http.StatusOK, message)
 }
